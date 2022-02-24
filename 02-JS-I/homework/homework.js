9e4c746aa292a72fdaf50090584862ee247c539b
david <david_vera120@hotmail.com>
2022-02-24 05:06:07 -0500

    final commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -182,7 +182,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
- var random = Math.random() * 2;
+ var random = Math.random();
  return random; 
 }
 
