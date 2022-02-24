5eb1ff84ad08f0dd9e8bbbe6999b04a90425e6e2
david <david_vera120@hotmail.com>
2022-02-24 04:42:52 -0500

    final commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -192,7 +192,7 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
  if(numero > 0){
-   var mensaje = 'es positivo';
+   var mensaje = 'Es positivo';
  }
  else if (numero == 0)
  {
@@ -200,7 +200,7 @@ function esPositivo(numero) {
  }
  else
  {
-   var mensaje = 'es negativo';
+   var mensaje = 'Es negativo';
  }
  return mensaje;
 }
@@ -225,28 +225,31 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-var saludo = 'hola' + ' ' + nombre + '!';
+var saludo = 'Hola' + ' ' + nombre + '!';
 return saludo;  
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+ var area = alto * ancho;
+ return area; 
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+ var perimetro = lado * 4;
+ return perimetro 
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+var area_triangulo = (base * altura) / 2;
+return area_triangulo;
 }
 
 
@@ -254,7 +257,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+ var dolares = euro * 1.20;
+ return dolares; 
 }
 
 
@@ -264,8 +268,21 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  var numeroletras = letra.length();
+if (numeroletras > 1){
+  var mensaje2 = 'Dato incorrecto';
 }
+else if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra =='u' )
+{
+      var mensaje2 = 'Es vocal';
+ }
+ else
+ {
+  var mensaje2 = 'Dato incorrecto';  
+ }
+ return mensaje2;
+}
+
 
 
 
