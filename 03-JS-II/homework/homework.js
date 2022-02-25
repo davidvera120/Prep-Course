ec4a4d7fe7fb7360a311c65f9502ef344bba8b81
david <david_vera120@hotmail.com>
2022-02-25 02:07:01 -0500

    final2 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -179,7 +179,7 @@ if ((num1 > num2) && (num1 > num3) && (num1 > 0)){
 }
 else if (num1 < 0 || num2 < 0 || num3 < 0)
 {
-  var msj8 = 'hay negativos';
+  var msj8 = 'Hay negativos';
 }
 else if ((num3 > num1) && (num3 > num2))
 {
@@ -187,7 +187,7 @@ var msj8 = num3 + 1;
 }
 else if (num1 === 0 || num2 === 0 || num3 === 0)
 {
-  var msj8 = 'Error'
+  var msj8 = 'Error';
 }
 else 
 {
@@ -218,9 +218,11 @@ var msj9 = true;
   {
  var msj9 = false;
   }
+  return msj9;
 }
 return msj9;
 }
+
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
@@ -240,29 +242,36 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
-}
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6
+} 
+
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if(numero > 99 && numero < 1000){
+    return true;
+  }
+  return false;
+}
 
-var digitos = numero.length;
-if (digitos === 3)
-{
-  var msj11 = true;
-}   
-else
-{
-var msj11 = false;
-}
-return msj11;
-}
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a;
 }
 
 
