277e9f057b96351757ab688097eccb092803455f
david <david_vera120@hotmail.com>
2022-02-24 22:06:09 -0500

    final2 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -150,18 +150,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-
- if (numero % 3 === 0){
-var msj7 = 'fizz';
+  if ((numero % 3 === 0)&&(numero % 5 === 0))
+  {
+  var msj7 = 'fizzbuzz';
+  }
+ else if (numero % 3 === 0){
+  var msj7 = 'fizz';
  } 
  else if (numero % 5 === 0){
    var msj7 = 'buzz';
  } 
- else if ((numero % 3 === 0)&&(numero % 5 === 0))
+ else
  {
-   var msj7 = 'fizzbuzz';
- }
- else{
    var msj7 = numero;
  }
 return msj7;
@@ -174,7 +174,7 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  
+
 }
 
 function esPrimo(numero) {
